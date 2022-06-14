dab709ca5bcc70caab407312d5a7be5e01b1d2c4
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-06-14 18:25:22 +0100

    initial commit

---
+++ Food/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Day = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(8, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(19, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "<"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(139, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(19, 35)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = ">"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Day
+        '
+        Me.Day.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Day.Location = New System.Drawing.Point(33, 12)
+        Me.Day.Name = "Day"
+        Me.Day.Size = New System.Drawing.Size(100, 35)
+        Me.Day.TabIndex = 2
+        Me.Day.Text = "Monday"
+        Me.Day.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(227, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(90, 35)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Setup"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(119, 53)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(90, 35)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Setup"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(383, 67)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 124)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(844, 377)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Day)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Day As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Food/Form1.vb
@@ -0,0 +1,102 @@
+﻿Public Class Form1
+
+    Dim daysOfWeek() As String = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+    Dim curDay As Integer = 0
+    Class meal
+        Public name As String
+        Public id As Integer
+        Public used As Boolean
+    End Class
+    Public Shared Function FisherYatesShuffle(data As Integer()) As Integer()
+        Dim retVal As Integer() = New Integer(data.Length - 1) {}
+        Dim ind As Integer() = New Integer(data.Length - 1) {}
+        Dim index As Integer
+        Dim rand As New Random()
+
+        For i As Integer = 0 To data.Length - 1
+            ind(i) = 0
+        Next
+        For i As Integer = 0 To data.Length - 1
+            Do
+                index = rand.[Next](data.Length)
+            Loop While ind(index) <> 0
+
+            ind(index) = 1
+            retVal(i) = data(index)
+        Next
+        Return retVal
+    End Function
+    Private Sub Day_Click(sender As Object, e As EventArgs) Handles Day.Click
+
+    End Sub
+
+    Private Sub changeDay(direction As Boolean)
+        Try
+            If (direction) Then
+                curDay += 1
+                Day.Text = daysOfWeek(curDay)
+            Else
+                curDay -= 1
+                If (curDay = -1) Then
+                    curDay = 6
+                    Day.Text = daysOfWeek(curDay)
+                Else
+                    Day.Text = daysOfWeek(curDay)
+                End If
+            End If
+        Catch ex As Exception
+            curDay = 0
+            Day.Text = daysOfWeek(curDay)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        changeDay(True)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        changeDay(False)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim numOfMeal As Integer
+
+        Try
+            numOfMeal = InputBox("How many many meals would u like to input?")
+            numOfMeal -= 1
+        Catch ex As Exception
+            MsgBox("Please Enter A Valid Response!")
+        End Try
+
+        Dim meals(numOfMeal) As meal
+        Dim data(numOfMeal) As Integer
+
+        For counter = 0 To numOfMeal
+            data(counter) = counter
+        Next
+
+        Dim retVal As Integer() = FisherYatesShuffle(data)
+
+        For counter = 0 To numOfMeal
+            meals(counter) = New meal
+
+            meals(counter).name = InputBox("Please Enter The name of the meal")
+            meals(counter).id = retVal(counter)
+        Next
+
+        For counter = 0 To numOfMeal
+            ListBox1.Items.Add(meals(counter).name)
+        Next
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim data() As Integer = {0, 1, 2, 3, 4, 5, 6}
+        Dim retVal As Integer() = FisherYatesShuffle(data)
+
+        For counter = 0 To 6
+            ListBox1.Items.Add(retVal(counter))
+        Next
+    End Sub
+End Class
